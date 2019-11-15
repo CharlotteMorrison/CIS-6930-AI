1af7d8697c3221802bf9022b11b087cee72da2ef
charlotte <charlottemaemorrison@gmail.com>
2019-11-14 20:28:54 -0500

    updated ui

--- Symposium/userinput.py
+++ Symposium/userinput.py
@@ -10,50 +10,46 @@ def sentiment_str(x):
         return 'Positive'
 
 
+def analyze(tweet):
+    nb = sentiment_str(model_NB.predict(tweet)[0])
+    sgd = sentiment_str(model_SGD.predict(tweet)[0])
+    svm = sentiment_str(model_SVM.predict(tweet)[0])
+    # add in a method for disagreement
+    return nb, sgd, svm
+
+
 if __name__ == "__main__":
     # load model(s)
     model_NB = joblib.load("pickles/naive_bayes.pkl")
-    model_SGD = joblib.load('pickles/sgd.pkl')
-    model_SVM = joblib.load('pickles/svm.pkl')
+    model_SGD = joblib.load("pickles/sgd.pkl")
+    model_SVM = joblib.load("pickles/svm.pkl")
 
     layout = [[sg.Text('Twitter Sentiment Analysis', size=(75, 1), font=('Helvetica', 24), justification='center')],
               [sg.Text('Enter a possible tweet (280 characters or less):', font=('Helvetica', 16)),
                sg.Input(key='_IN_', do_not_clear=False, size=(75, 5), font=('Helvetica', 16))],
-              [sg.Text('', size=(80, 1)), sg.Button('Analyze', font=('Helvetica', 16))],
+              [sg.Text(size=(80, 1)), sg.Button('Analyze', font=('Helvetica', 16))],
               [sg.Text('Tweet sentiment according to classification models:', font=('Helvetica', 16))],
-              [sg.Text('Your tweet: ', font=('Helvetica', 16)),
-               sg.Text('', font=('Helvetica', 16), text_color='blue', key='_TWEET_OUT_')],
-              [sg.Text("Naives-Bayes", font=('Helvetica', 16)),
-               sg.Text("Stochastic Gradient Descent", font=('Helvetica', 16)),
-               sg.Text("Support Vector", font=('Helvetica', 16))],
-              [sg.Text(size=(33, 5), font=('Helvetica', 16), key='_NB_Output_'),
-               sg.Text(size=(33, 5), font=('Helvetica', 16), key='_SGD_Output_'),
-               sg.Text(size=(33, 5), font=('Helvetica', 16), key='_SVM_Output_')]]
+              [sg.Text(font=('Helvetica', 16),  size=(75, 2), text_color='blue', key='_TWEET_OUT_')],
+              [sg.Text('Naives-Bayes                      ', size=(33, 1), font=('Helvetica', 16)),
+               sg.Text('Stochastic Gradient Descent       ', size=(33, 1),  font=('Helvetica', 16)),
+               sg.Text('Support Vector                    ', size=(33, 1), font=('Helvetica', 16))],
+              [sg.Text(size=(33, 2), font=('Helvetica', 16), key='_NB_Output_'),
+               sg.Text(size=(33, 2), font=('Helvetica', 16), key='_SGD_Output_'),
+               sg.Text(size=(33, 2), font=('Helvetica', 16), key='_SVM_Output_')]]
 
     window = sg.Window('Tweet Sentiment Analysis', layout)
-
     while True:  # Event Loop
         event, values = window.read()  # can also be written as event, values = window()
-        print(event, values)
         if event is None or event == 'Exit':
             break
-        if event == 'Show':
-            # get and format the prediction
-            tweet = str([values['_IN_']])
-            print(tweet)
-            p1 = model_NB.predict(tweet)
-            p2 = model_SGD.predict(tweet)
-            p3 = model_SVM.predict(tweet)
-            result_p1 = sentiment_str(p1[0])
-            result_p2 = sentiment_str(p2[0])
-            result_p3 = sentiment_str(p3[0])
-            print(result_p1)
-            print(result_p2)
-            print(result_p3)
-            # change the "output" element to be the value of "input" element
-            window['_TWEET_OUT_'].update(tweet)
-            window['_NB_Output_'].update(result_p1)
-            window['_SGD_Output_'].update(result_p2)
-            window['_SVM_Output_'].update(result_p3)
+        if event == 'Analyze':
+            tweet_in = [values['_IN_']]
 
+            p1, p2, p3 = analyze(tweet_in)
+
+            tweet_out = result = "Your Tweet: '{}'".format(str(values['_IN_']))
+            window['_TWEET_OUT_'].update(tweet_out)
+            window['_NB_Output_'].update(p1)
+            window['_SGD_Output_'].update(p2)
+            window['_SVM_Output_'].update(p3)
     window.close()
