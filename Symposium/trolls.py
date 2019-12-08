0da71eeb9a192ae5529ff86ff40586801429e799
charlotte <charlottemaemorrison@gmail.com>
2019-12-08 08:30:14 -0500

    added Russian troll tweets average sentiment

--- Symposium/trolls.py
+++ Symposium/trolls.py
@@ -1,5 +1,5 @@
+from datetime import date
 from dateutil.parser import parse
-
 import joblib
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -7,38 +7,45 @@ from Symposium.dataloader import load_dataset
 from Symposium.preprocessing import process_tweet, text_process, stem
 
 if __name__ == "__main__":
-
-    datafile = "dataset/IRAhandle_tweets_1.csv"
-    columns = ["external_author_id", "content", "account_type", "publish_date"]
-
-    dataset = load_dataset(datafile, columns)
-
-    # remove the time from date/time, convert to date format
-    dataset['publish_date'] = [parse(text.split(' ', 1)[0]) for text in dataset['publish_date']]
-
-    # clean the dataset
-    dataset['content'] = dataset['content'].apply(process_tweet)
-
     # load model(s)
-    model_NB = joblib.load("pickles/naive_bayes.pkl")
+    # model_NB = joblib.load("pickles/naive_bayes.pkl")
     # model_SGD = joblib.load("pickles/sgd.pkl")
-    # model_SVM = joblib.load("pickles/svm.pkl")
+    model_SVM = joblib.load("pickles/svm.pkl")
 
-    # produce predictions
-    dataset['score'] = model_NB.predict(dataset['content'])
+    dataset_graph = pd.Series()
+    for x in range(1, 13):
+        datafile = "dataset/IRAhandle_tweets_{}.csv".format(x)
+        columns = ["content", "publish_date", "language"]
 
-    data_group = dataset.groupby(['publish_date'])['score'].mean()
+        dataset = load_dataset(datafile, columns)
+
+        # the model is trained on english, so only keep the English tweets
+        dataset = dataset[dataset.language.str.contains('English')]
+
+        # remove the time from date/time, convert to date format
+        dataset['publish_date'] = [parse(text.split(' ', 1)[0]) for text in dataset['publish_date']]
+
+        # limit to tweets after 12/1/2015
+        dataset = dataset[dataset['publish_date'] > pd.Timestamp(date(2015, 1, 1))]
+
+        # clean the dataset
+        dataset['content'] = dataset['content'].apply(str)
+        dataset['content'] = dataset['content'].apply(process_tweet)
+
+        # produce predictions
+        dataset['score'] = model_SVM.predict(dataset['content'])
+
+        dataset_graph = pd.concat([dataset_graph, dataset], axis=0, sort=True)
+
+        print("Processed dataset {}".format(x))
+
+    data_mean = dataset_graph.groupby(['publish_date'])['score'].mean()
     plt.style.use('ggplot')
     fig, ax = plt.subplots(figsize=(15, 7))
-    data_group.plot(ax=ax)
-    # dataset.groupby(['publish_date']).count()['score'].plot(ax=ax)
+    data_mean.plot(ax=ax)
     ax.set_xlabel('Date')
     ax.set_ylabel('Mean Sentiment')
     plt.title('Troll Tweet Mean Sentiment')
     plt.show()
     fig.savefig('graphs/tweets_1_NB.png')
     plt.close('all')
-
-    # nb = model_NB.predict(tweet)[0]
-    # sgd = model_SGD.predict(tweet)[0]
-    # svm = model_SVM.predict(tweet)[0]
